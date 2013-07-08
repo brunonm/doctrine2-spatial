2825beea1e3f43140a6d5bffb3ed350ac09d4b47
brunonm <>
2013-07-08 20:11:41 -0300

    [DEL] - remoção dos recursos que não serão utilizados
    [UPDATE]
    - alteração do GeometryType

--- INSTALL.md
+++ INSTALL.md
@@ -3,7 +3,7 @@
 ## composer.json
     "require": {
     	...
-        "creof/doctrine2-spatial": "dev-master"
+        "ue/doctrine2-spatial": "dev-master"
 
 You will also have to change the version requirement of doctrine to at least 2.1:
 
@@ -16,15 +16,12 @@ You need to manually add the types and functions you use:
 	doctrine:
 	    dbal:
 	        types:
-	            geometry:   CrEOF\Spatial\DBAL\Types\GeometryType
-	            point:      CrEOF\Spatial\DBAL\Types\Geometry\PointType
-	            polygon:    CrEOF\Spatial\DBAL\Types\Geometry\PolygonType
-	            linestring: CrEOF\Spatial\DBAL\Types\Geometry\LineStringType
+	            geometry:   Ue\Spatial\DBAL\Types\GeometryType
 	    orm:
 	        dql:
 	            numeric_functions:
-	                st_contains:        CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STContains
-	                st_distance:        CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STDistance
-	                st_area:            CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STArea
-	                st_length:          CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STLength
-	                st_geomfromtext:    CrEOF\Spatial\ORM\Query\AST\Functions\PostgreSql\STGeomFromText
+	                st_contains:        Ue\Spatial\ORM\Query\AST\Functions\PostgreSql\STContains
+	                st_distance:        Ue\Spatial\ORM\Query\AST\Functions\PostgreSql\STDistance
+	                st_area:            Ue\Spatial\ORM\Query\AST\Functions\PostgreSql\STArea
+	                st_length:          Ue\Spatial\ORM\Query\AST\Functions\PostgreSql\STLength
+	                st_geomfromtext:    Ue\Spatial\ORM\Query\AST\Functions\PostgreSql\STGeomFromText
---
+++ lib/Ue/Spatial/DBAL/Types/GeometryType.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Ue\Spatial\DBAL\Types;
+
+use Doctrine\DBAL\Types\Type,
+    Doctrine\DBAL\Types\ConversionException,
+    Doctrine\DBAL\Platforms\AbstractPlatform;
+
+/**
+ * Class for database column "geometry".
+ *
+ */
+class GeometryType extends Type {
+    const GEOMETRY = 'geometry';
+    const SRID = 4674;
+
+    /**
+     *
+     * @param array $fieldDeclaration
+     * @param AbstractPlatform $platform
+     * @return string
+     */
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform) {
+        return 'geometry';
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Doctrine\DBAL\Types\Type::convertToPHPValue()
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform) {
+        return $value;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Doctrine\DBAL\Types\Type::convertToDatabaseValue()
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform) {
+        return $value;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Doctrine\DBAL\Types\Type::getName()
+     */
+    public function getName() {
+        return self::GEOMETRY;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Doctrine\DBAL\Types\Type::canRequireSQLConversion()
+     */
+    public function canRequireSQLConversion() {
+        return true;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Doctrine\DBAL\Types\Type::convertToPHPValueSQL()
+     */
+    public function convertToPHPValueSQL($sqlExpr, $platform) {
+        return 'ST_AsText(\''.$sqlExpr.'\') ';
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Doctrine\DBAL\Types\Type::convertToDatabaseValueSQL()
+     */
+    public function convertToDatabaseValueSQL($sqlExpr, AbstractPlatform $platform) {
+        return 'ST_GeomFromText('.$sqlExpr.', '.self::SRID.')';
+    }
+}
